88ec7d2a20735a5d90130dfd99a6b4d6ecd5e7b2
Herbert Suantak <108845744+HBMods-OFC@users.noreply.github.com>
2024-02-27 19:57:10 +0530

    Fix update

--- settings.js
+++ settings.js
@@ -7,8 +7,12 @@ global.GroupWcMsg = true // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
 global.available = true // false a i dah chuan offline angin a awm ang
+global.public = true // false ah i dah chuan nang chauhin i hmang thei ang
 global.autoread = false // true a i dah chuan message zawng zawng read vek zelin a awm ang 
 global.autoread_status = false // true a i dah chuan auto status view in a awm ang
+global.default_language = true // default language i hman dawn loh chuan false ah hia  dah tur
+global.mtl_language = false // multi language i hman dawn chuan true ah dah tur
+global.bot_language = 'lus' // I language code dah tur code hman i hriat loh chuan https://herbert70.blogspot.com/p/language-code-for-hbwabot.html?m=1 he link ah hian en thei a ni ang
 
 //info
 global.owner = ['918416093656'] // I phone number in thlak rawh
